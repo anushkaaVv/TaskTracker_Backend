c3754eb2385246da864da259232b1aaac3126a58
anushkaaVv <anushka_verma@optum.com>
2024-06-04 21:53:21 +0530

    more validations added

--- src/main/java/com/TaskTracker/TaskTracker_backend/dto/TaskDto.java
+++ src/main/java/com/TaskTracker/TaskTracker_backend/dto/TaskDto.java
@@ -1,10 +1,16 @@
 package com.TaskTracker.TaskTracker_backend.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
+
 import java.time.LocalDate;
+import java.util.Date;
 
 @Getter
 @Setter
@@ -15,12 +21,18 @@ public class TaskDto {
 
 
     private  Long id;
-    @NotEmpty
+
+    @NotBlank(message = "Task name cannot be blank")
+    @Length(min = 3, max = 30, message = "Task name must be between 3-15 characters")
     private  String taskName;
-    @NotEmpty
+
+
+    @NotBlank(message = "Status cannot be blank")
     private String status;
 
     @NotNull
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate date;
+
 
 }
--- src/main/java/com/TaskTracker/TaskTracker_backend/entity/Task.java
+++ src/main/java/com/TaskTracker/TaskTracker_backend/entity/Task.java
@@ -10,6 +10,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
 
 
 @Getter
